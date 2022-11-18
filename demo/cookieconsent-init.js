9c0ecc54eeb8de6c0e6d5b7ed8789f15402cb835
Nilotpaldeb <nilofresh@gmail.com>
2022-11-18 12:13:56 +0530

    Update cookieconsent-init.js

--- demo/cookieconsent-init.js
+++ demo/cookieconsent-init.js
@@ -1,7 +1,5 @@
-// obtain cookieconsent plugin
 var cc = initCookieConsent();
 
-// run plugin with config object
 cc.run({
     current_lang: 'en',
     autoclear_cookies: true,
@@ -45,7 +43,7 @@ cc.run({
         'en': {
             consent_modal: {
                 title: 'Hello traveller, it\'s cookie time!',
-                description: 'Our website uses essential cookies to ensure its proper operation and tracking cookies to understand how you interact with it. The latter will be set only after consent. <a href="#privacy-policy" class="cc-link">Privacy policy</a>',
+                description: 'Our website uses essential cookies to ensure its proper operation and tracking cookies to understand how you interact with it. The latter will be set only after consent. <a href="https://theteer.com/privacy/" class="cc-link">Privacy policy</a>',
                 primary_btn: {
                     text: 'Accept all',
                     role: 'accept_all'
